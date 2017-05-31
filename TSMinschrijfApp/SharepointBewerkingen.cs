a0ebf77cb924eb237a7e6cc67d410ae1f572a3c1
Herman Messagie <herman.messagie@telenet.be>
2017-05-31 10:54:44 +0200

    aangemaakte leerling direct openen in sharepoint indien gewenst om inschrijving verder af te werken

--- TSMinschrijfApp/SharepointBewerkingen.cs
+++ TSMinschrijfApp/SharepointBewerkingen.cs
@@ -58,7 +58,43 @@ namespace TSMinschrijfApp
             //leerling bestaat nog niet
             return false;
         }
-
+        public String VraagContentId(Leerling teControlerenLeerling)
+        {
+            var securePassword = new SecureString();
+            foreach (var c in paswoord)
+            {
+                securePassword.AppendChar(c);
+            }
+            credentials = new SharePointOnlineCredentials(userName, securePassword);
+            //context = new ClientContext("https://technischescholenmechel.sharepoint.com/TSM ICT/");
+            context = new ClientContext("https://technischescholenmechel.sharepoint.com/TSM Globaal/");
+            context.Credentials = credentials;
+            // Het sharepoint web binnenhalen.
+            Web web = context.Web;
+            // De bestaande lijst die we nodig hebben binnenhalen "Testlijst - Inschrijvingen eID". 
+            // List announcementsList = context.Web.Lists.GetByTitle("Testlijst - Inschrijvingen eID");
+            List announcementsList = context.Web.Lists.GetByTitle("MS Inschrijvingen - TEST");
+            // This creates a CamlQuery that has a RowLimit of 100, and also specifies Scope="RecursiveAll" 
+            // so that it grabs all list items, regardless of the folder they are in. 
+            CamlQuery query = CamlQuery.CreateAllItemsQuery(100);
+            ListItemCollection items = announcementsList.GetItems(query);
+            // Retrieve all items in the ListItemCollection from List.GetItems(Query). 
+            context.Load(items);
+            context.ExecuteQuery();
+            MessageBox.Show("Tot hier geraak ik");
+            foreach (ListItem listItem in items)
+            {
+                
+                // juiste leerling opzoeken op bass van nationaalnummer. 
+                if (teControlerenLeerling.nationaalnummer.Equals(listItem["mofc"]))
+                {
+                    //leerling ID terug sturen voor opbouw URL
+                    return listItem["ID"].ToString();
+                }
+            }
+            //Moest leerling toch niet gevonden worden volgende terug sturen.
+            return "Leerling niet gevonden";
+        }
         public void  LeerlingBewarenInSharepointList(Leerling teBewarenLeerling,String schooljaar)
         {
             
@@ -130,7 +166,14 @@ namespace TSMinschrijfApp
             DialogResult dResult = MessageBox.Show("Leerling is aangemaakt. Verder gaan op inschrijvingssite ?", "Inschrijving resultaat", MessageBoxButtons.OKCancel);
             if (dResult == DialogResult.OK)
             {
-                Process.Start("https://technischescholenmechel.sharepoint.com/TSM%20Globaal/Lists/MS%20Inschrijvingen%20%20TEST/Overzicht.aspx");
+                if (VraagContentId(teBewarenLeerling) == "Leerling niet gevonden")
+                {
+                    String url = "https://technischescholenmechel.sharepoint.com/TSM%20Globaal/Lists/MS%20Inschrijvingen%20%20TEST/Item/editifs.aspx?List=fac6218d%2D5da6%2D4dd4%2D9f27%2Dd51e2306b156&ID=" + VraagContentId(teBewarenLeerling) + "&Source=https%3A%2F%2Ftechnischescholenmechel%2Esharepoint%2Ecom%2FTSM%2520Globaal%2FLists%2FMS%2520Inschrijvingen%2520%2520TEST%2FOverzicht%2Easpx&ContentTypeId=0x0100F6FB02CA6D57EC41B63B32BBDE8F5824";
+                    Process.Start(url);
+                } else
+                {
+                    MessageBox.Show("Leerling niet gevonden om Sharepoint te openen.","Melding");
+                }
             }
         }
     }
